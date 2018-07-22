3c4708449dde81fcab881d300c125822e72cebf1
Corey Castillo <mcguiser@users.noreply.github.com>
2018-07-22 14:08:18 -0500

    Cascade delete demo

---
+++ src/com/corey/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,63 @@
+package com.corey.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.corey.hibernate.demo.entity.Instructor;
+import com.corey.hibernate.demo.entity.InstructorDetail;
+import com.corey.hibernate.demo.entity.Student;
+
+public class DeleteInstructorDetailDemo {
+
+	public static void main(String[] args) {
+
+		// Create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// Create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+
+			// Start a transaction
+			session.beginTransaction();
+			
+			// Get the instructor detail object
+			int theId = 1;
+			InstructorDetail tempInstructorDetail = 
+					session.get(InstructorDetail.class, theId);
+			
+			// Print the instructor detail
+			System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+			
+			// Print the associated instructor
+			System.out.println("the assoiciated instructor: " + 
+								tempInstructorDetail.getInstructor());
+			
+			// Delete the instructor detail
+			System.out.println("Deleting tempInstructorDetail:" + tempInstructorDetail);
+			session.delete(tempInstructorDetail);
+			
+			// Commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done!");
+			
+		}
+		catch (Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			// Handle connection leak
+			session.close();
+			
+			factory.close();
+		}
+
+	}
+
+}
